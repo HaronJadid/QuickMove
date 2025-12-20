597d859d5aff3f4e2b4886f2edac9e1968dc3be0
mohssine el addaoui <mohssineeladdaoui2004@gamil.com>
2025-12-20 16:28:38 +0100

    adding the login methode

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -8,4 +8,11 @@ const authController = require('../controllers/authController');
 // POST /api/auth/register
 router.post('/register', authController.registerUser);
 
+// POST /api/auth/login
+router.post('/login', authController.login);
+
+// Password Reset
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
+
 module.exports = router;
